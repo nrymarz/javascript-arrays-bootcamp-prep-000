34560c48b2c19fcecea5fb5c842d2327afc0ea61
Nathan Rymarz <nrymarz@gmail.com>
2020-10-09 19:54:41 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -1,7 +1,18 @@
 function addElementToBeginningOfArray(arr,element){
-  var a = arr.unshift(element);
+  var a = [element,...arr];
   return a;
 }
 function destructivelyAddElementToBeginningOfArray(arr,element){
   return arr.unshift(element);
+}
+var chocolateBars = ["snickers","hundred grand","kitkat","skittles"]
+function addElementToEndOfArray(arr,element){
+  newArray = [arr...,element];
+  return newArray;
+}
+function destructivelyAddElementToEndOfArray(arr,element){
+  return arr.push(element);
+}
+function accessElementInArray(arr,index){
+  return arr[index];
 }
